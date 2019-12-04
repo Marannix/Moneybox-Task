822ce63d43ee124c152b2adbfa7ea420035e3d03
Josh <onabanjotobi@gmail.com>
2019-12-04 19:29:38 +0000

    Created a card view layout to display the plan information

--- app/src/main/java/com/example/minimoneybox/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/minimoneybox/activity/DashboardActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.minimoneybox.R
 import com.example.minimoneybox.User
+import com.google.android.material.card.MaterialCardView
 
 class DashboardActivity : BaseActivity() {
 
@@ -16,5 +17,10 @@ class DashboardActivity : BaseActivity() {
             val user : User = extras.getParcelable("user")
             Log.d("Dashboard", user.firstName)
         }
+
+        val stockAndShareLayout = findViewById<MaterialCardView>(R.id.stockAndShareLayout)
+        val planValueLabel = findViewById<MaterialCardView>(R.id.generalInvestmentAccountLayout)
+        val moneyBoxLabel = findViewById<MaterialCardView>(R.id.lifetimeISALayout)
+
     }
 }
